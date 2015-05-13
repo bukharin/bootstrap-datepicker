21058755f0a6a7e5b2294ce6c89a3cacb71a3970
Hugo Espresati <hespresati@gmail.com>
2015-05-13 14:15:43 +0200

    Added week start and format

--- js/locales/bootstrap-datepicker.es.js
+++ js/locales/bootstrap-datepicker.es.js
@@ -1,6 +1,8 @@
 /**
  * Spanish translation for bootstrap-datepicker
  * Bruno Bonamin <bruno.bonamin@gmail.com>
+ * 
+ * Update with week start and date format: Hugo Espresati <hespresati@gmail.com>
  */
 ;(function($){
 	$.fn.datepicker.dates['es'] = {
@@ -9,6 +11,8 @@
 		daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
 		months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
 		monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
-		today: "Hoy"
+		today: "Hoy",
+        	weekStart: 1,
+        	format: "d/m/yyyy"
 	};
 }(jQuery));
